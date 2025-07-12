5a5139ebba89954768f94c99cbe13ddbf9f027ea
JuniorDCoder <foryoungjuniorngu@gmail.com>
2025-07-12 09:03:32 +0100

    feat: Fix admin migration

---
+++ database/migrations/2025_07_12_080239_add_specialty_id_to_interns_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('interns', function (Blueprint $table) {
+            $table->foreignId('specialty_id')->after('user_id')->constrained('specialties')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('interns', function (Blueprint $table) {
+            $table->dropForeign(['specialty_id']);
+            $table->dropColumn('specialty_id');
+        });
+    }
+};
