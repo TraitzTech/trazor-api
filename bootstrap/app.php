e79566ee83a7b5a2b4e25f5cb6f93a4cafcf0b20
JuniorDCoder <foryoungjuniorngu@gmail.com>
2026-01-16 22:32:41 +0100

    feat: Attemp to solve cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors; // Add this import
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Prepend the HandleCors middleware to the global stack
+        // This ensures it runs before any other middleware or route logic
+        $middleware->prepend(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
