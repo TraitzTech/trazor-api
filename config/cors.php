625d181060c07ddb23a342778fca565ab05a0f78
JuniorDCoder <foryoungjuniorngu@gmail.com>
2026-01-16 13:34:47 +0100

    fix: Update cors issues

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000, https://trazor.traitz.tech/api'],
 
     'allowed_origins_patterns' => [],
 
