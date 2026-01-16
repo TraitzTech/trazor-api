f8cb8dab88ac2bee554bea9f5d3e7fedafaf4c5a
JuniorDCoder <foryoungjuniorngu@gmail.com>
2026-01-16 23:01:44 +0100

    feat: Install sramble for auto api documentation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Allow access to API documentation for all users
+        Scramble::ignoreDefaultRoutes();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// API Documentation routes
+Scramble::registerUiRoute('docs/api');
+Scramble::registerJsonSpecificationRoute('docs/api.json');
